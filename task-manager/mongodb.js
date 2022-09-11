4d7147d87ba7b3910e3b615981156c8ad5ac0be5
NivasDonthireddy <dlnivas@hotmail.com>
2022-09-11 21:48:55 +0530

    update operation

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -4,18 +4,13 @@ const dbName = 'task-manager';
 
 MongoClient.connect(connectionUrl,(err,client)=>{
     const db = client.db(dbName);
-    const updatePromise = db.collection('users').updateOne({
-        _id:new ObjectId("631db18acf5b5a37070fe2b6")
-    },{
-        $inc: {
-            age:1
+    db.collection('tasks').updateMany({completed: false},{
+        $set: {
+            completed: true
         }
-    });
-
-    updatePromise.then((result)=>{
+    }).then((result)=>{
         console.log(result);
-    }).catch((error)=>{
-        console.log(error);
+    }).catch((err)=>{
+        console.log(err);
     })
-
 });
