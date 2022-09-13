81eb2a08e90b614e72394c9cad4fe4e4f3b78f84
NivasDonthireddy <dlnivas@hotmail.com>
2022-09-13 12:23:08 +0530

    async await conceptual levle

--- task-manager/playground/promise-chaining.js
+++ task-manager/playground/promise-chaining.js
@@ -1,11 +1,14 @@
 require('../src/db/mongoose');
 
+const { findByIdAndUpdate } = require('../src/models/user');
 const User = require('../src/models/user');
 
-User.findByIdAndUpdate('631f6bc57f602dfe3224f7bf',{age: 1})
-            .then(()=>User.countDocuments({age: 1}))
-            .then((result)=>{
-                console.log(result);})
-            .catch((e)=>{
-                console.log(e);
-            });
+const updateAgeAndCount = async (id,age) => {
+    const user = await User.findByIdAndUpdate(id,{age});
+    const count = await User.countDocuments({age});
+    return count;
+}
+
+updateAgeAndCount('631f621b152499361d9cd3a3',2)
+    .then((result)=>console.log(result))
+    .catch((err)=>console.log(err));
